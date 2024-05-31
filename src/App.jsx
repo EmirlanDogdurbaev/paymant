6828eaa82864c6ca516412c67cf8356f9066ec3d
darkbala <rustam.samsaev@gmail.com>
2024-06-01 05:45:56 +0600

    not not

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import './App.css'
-import {Route, Routes} from "react-router-dom";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./module/Layout/Layout.jsx";
+import Signup from "./module/Auth/Signup/Signup.jsx";
+import Login from "./module/Auth/Login/Login.jsx";
 import HomePage from "./pages/HomePage.jsx";
 
 function App() {
-
-    return (
-        <>
-            <Layout>
-                <Routes>
-                    <Route path="/" exact element={<HomePage/>}/>
-                </Routes>
-            </Layout>
-        </>
-    )
+  return (
+    <>
+      <Layout>
+        {localStorage.getItem("token") ? (
+          <Routes>
+            <Route path="/register" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        )}
+      </Layout>
+    </>
+  );
 }
 
-export default App
+export default App;
