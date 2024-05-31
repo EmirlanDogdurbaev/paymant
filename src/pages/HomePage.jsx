0e97d251f57c4e334aa1b05906cff5a7b3c4157f
darkbala <rustam.samsaev@gmail.com>
2024-05-31 21:47:34 +0600

    add footer component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
-
-import React from 'react'
 import Video from '../components/Video/Video'
 import classes from './HomePage.module.scss'
 import BackgroundSpot from '../components/BackgroundSpot/BackgroundSpot'
+import Footer from '../components/Footer/Footer'
 
 const HomePage = () => {
     return (
         <div className={classes.HomePage}>
             <Video/>
             <BackgroundSpot/>
+            <Footer/>
         </div>
     )
 }
