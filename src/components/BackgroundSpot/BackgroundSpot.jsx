4f1661f07b688f6f4ddcd8b30711076240005768
AsanbaevBaitemir <baitemir1706@gmail.com>
2024-05-31 23:06:28 +0600

    removed spot

--- src/components/BackgroundSpot/BackgroundSpot.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import classes from './BackgroundSpot.module.scss'
-import spot from './back test.svg'
-export default function BackgroundSpot() {
-  return (
-    <div style={{backgroundImage:`url(${spot})`}} className={classes.BackgroundSpot}></div>
-  )
-}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
 import Video from '../components/Video/Video'
 import classes from './HomePage.module.scss'
-import BackgroundSpot from '../components/BackgroundSpot/BackgroundSpot'
 import Footer from '../components/Footer/Footer'
 import HowItWorks from '../components/HowItWorks/HowItWorks'
+import Comments from '../components/Comments/Comments'
 
 const HomePage = () => {
     return (
         <div className={classes.HomePage}>
-            <Video/>
-            <BackgroundSpot/>
+            
             <HowItWorks/>
+            <Video/>
+            <Comments/>
             <Footer/>
         </div>
     )
