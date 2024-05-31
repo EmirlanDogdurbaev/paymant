7be9c1bbace17f752406af02f6226c699e04af00
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-05-31 20:21:54 +0600

    added register and configurate file system

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
-import Layout from "./components/Layout/Layout.jsx";
+import Layout from "./module/Layout/Layout.jsx";
 
 function App() {
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A iste quasi quos. Excepturi facere neque
+                suscipit vel. Accusantium consequuntur doloribus dolorum eum excepturi reiciendis repellat, similique!
+                Accusamus nostrum pariatur totam?
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
