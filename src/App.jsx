ec9dc616e820ebb1417b38a2a0fd9bad04bed0d2
Aidarbek Kurmankozhoev <aidarbek.kurmankojoev.pls@gmail.com>
2024-05-31 21:05:58 +0600

    header is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Layout from "./module/Layout/Layout.jsx";
+import Header from './components/Header/Header.jsx';
 
 function App() {
 
   return (
     <>
+    <Header/>
      <Layout>
        <Routes>
          <Route path="/" exact comonent={App} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,41 @@
+import { Link } from "react-router-dom";
+import  classes from "./Header.module.scss"
+
+
 const Header = () => {
     return (  
-        <div className="Header">
-            
+        <div className={classes.Header}>
+            <div className="container">
+                <div className={classes.Header__inner}>
+                    <Link className={classes.Header__logo} to="/">
+                        Logo
+                    </Link>
+                    <nav className={classes.navbar}>
+                        <ul className={classes.navbar__list}>
+                            <li className={classes.navbar__item}>
+                                <Link className={classes.navbar__link} to="/">
+                                    Главная
+                                </Link>
+                            </li>
+                            <li className={classes.navbar__item}>
+                                <Link className={classes.navbar__link} to="/">
+                                    О нас
+                                </Link>
+                            </li>
+                            <li className={classes.navbar__item}>
+                                <Link className={classes.navbar__link} to="/">
+                                    Контакты
+                                </Link>
+                            </li>
+                            <li className={classes.navbar__item}>
+                                <Link className={classes.login} to="/">
+                                    Войти
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
     );
 }
