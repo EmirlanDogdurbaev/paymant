342cf13edf7366913982d9744baf48b35d7b1394
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-05-31 21:46:24 +0600

    axax

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Layout from "./module/Layout/Layout.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import HomePage from './pages/HomePage.jsx';
 
 function App() {
 
---
+++ src/components/Tools/Tools.jsx
@@ -0,0 +1,13 @@
+import classes from "./Tools.module.scss";
+
+const Tools = () => {
+    return (
+        <div className={classes.Tools}>
+            <section className='container'>
+
+            </section>
+        </div>
+    );
+};
+
+export default Tools;
