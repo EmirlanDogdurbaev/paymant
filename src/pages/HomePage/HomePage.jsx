8c48c1b219a378fb1e8832797085ea78a36039f6
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-06-01 05:36:22 +0600

    fix imports

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,11 @@
 import Video from '../../components/Video/Video'
 import classes from './HomePage.module.scss'
-import Footer from '../../components/Footer/Footer'
-import Hero from '../components/Hero/Hero'
-import HowItWorks from '../../components/HowItWorks/HowItWorks'
-import Comments from '../../components/Comments/Comments'
-import Tools from "../components/Tools/Tools.jsx";
-import About from '../components/About/About.jsx'
+import Hero from "../../components/Hero/Hero.jsx";
+import About from "../../components/About/About.jsx";
+import Tools from "../../components/Tools/Tools.jsx";
+import HowItWorks from "../../components/HowItWorks/HowItWorks.jsx";
+import Comments from "../../components/Comments/Comments.jsx";
+import {Footer} from "antd/es/modal/shared.js";
 
 const HomePage = () => {
     return (
