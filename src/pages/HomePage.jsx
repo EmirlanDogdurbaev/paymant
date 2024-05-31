0e6a7ecdb4936377bd8aa380d1e497adddec4341
darkbala <rustam.samsaev@gmail.com>
2024-05-31 22:33:40 +0600

    add howItWorks component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import Video from '../components/Video/Video'
 import classes from './HomePage.module.scss'
 import BackgroundSpot from '../components/BackgroundSpot/BackgroundSpot'
 import Footer from '../components/Footer/Footer'
+import HowItWorks from '../components/HowItWorks/HowItWorks'
 
 const HomePage = () => {
     return (
         <div className={classes.HomePage}>
             <Video/>
             <BackgroundSpot/>
+            <HowItWorks/>
             <Footer/>
         </div>
     )
