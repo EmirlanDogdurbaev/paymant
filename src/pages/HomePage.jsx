e5563cafd5beb619b89f536fe351cf0c3731eee2
AsanbaevBaitemir <baitemir1706@gmail.com>
2024-05-31 22:05:34 +0600

    comments

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Video from '../components/Video/Video'
 import classes from './HomePage.module.scss'
 import BackgroundSpot from '../components/BackgroundSpot/BackgroundSpot'
+import Comments from '../components/Comments/Comments'
 
 const HomePage = () => {
     return (
         <div className={classes.HomePage}>
             <Video/>
-            <BackgroundSpot/>
+            <Comments/>
+            {/* <BackgroundSpot/> */}
         </div>
     )
 }
