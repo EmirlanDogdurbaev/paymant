1b0632309933b68b1bf6acffd51d605936054a0d
AsanbaevBaitemir <baitemir1706@gmail.com>
2024-05-31 21:32:51 +0600

    little fix during merge

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
     return (
         <div className={classes.HomePage}>
             <Video/>
-        <BackgroundSpot/>
+            <BackgroundSpot/>
         </div>
     )
 }
