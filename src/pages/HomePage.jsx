2e54fef9b8e3f86cd09c200d9a30157b24b8a784
AsanbaevBaitemir <baitemir1706@gmail.com>
2024-05-31 21:28:55 +0600

    spot

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Video from '../components/Video/Video'
 import classes from './HomePage.module.scss'
+import BackgroundSpot from '../components/BackgroundSpot/BackgroundSpot'
 export default function HomePage() {
   return (
     <div className={classes.HomePage}>
         <Video/>
+        <BackgroundSpot/>
     </div>
   )
 }
