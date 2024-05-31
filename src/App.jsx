4cd11f43e153892eed769170e49232b37fe2b1b0
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-06-01 05:33:31 +0600

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./module/Layout/Layout.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import TransactionPage from './pages/TransactionPage/TransactionPage.jsx';
+import Signup from "./module/Auth/Signup/Signup.jsx";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
                 <Routes>
                     <Route path="/" exact element={<HomePage/>}/>
                     <Route path="/transaction" exact element={<TransactionPage/>}/>
+                    <Route path="/signup" element={<Signup/>}/>
                 </Routes>
             </Layout>
         </>
