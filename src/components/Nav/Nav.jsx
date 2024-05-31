d631ef7c24ac7f681306e8dcb97ad3a40773cf2c
Iimanbek <imanbek.akylbekov2017@gmail.com>
2024-06-01 02:08:28 +0600

    chala burger

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,12 @@
-import {Link} from "react-router-dom";
-import classes from "./Nav.module.scss";
+
+
+// src/Nav.js
+import { Link } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
+import burger from '../../assets/static/icons/menu.svg';
+import closer from '../../assets/static/icons/x.svg';
+import classes from './Nav.module.scss';
+
 const Nav = () => {
     return (
         <nav className={classes.navbar}>
@@ -10,21 +17,41 @@ const Nav = () => {
                     </Link>
                 </li>
                 <li className={classes.navbar__item}>
-                    <Link className={classes.navbar__link} to="/">
+                    <Link className={classes.navbar__link} to="/about">
                         О нас
                     </Link>
                 </li>
                 <li className={classes.navbar__item}>
-                    <Link className={classes.navbar__link} to="/">
+                    <Link className={classes.navbar__link} to="/contact">
                         Контакты
                     </Link>
                 </li>
                 <li className={classes.navbar__item}>
-                    <Link className={classes.login} to="/">
+                    <Link className={classes.login} to="/login">
                         Войти
                     </Link>
                 </li>
             </ul>
+            <div className={classes.navbar__burger}>
+                <Menu
+                    right
+                    customBurgerIcon={<img src={burger} alt="Menu" />}
+                    customCrossIcon={<img src={closer} alt="Close" />}
+                >
+                    <Link id="home" className="menu-item" to="/">
+                        Главная
+                    </Link>
+                    <Link id="about" className="menu-item" to="/about">
+                        О нас
+                    </Link>
+                    <Link id="contact" className="menu-item" to="/contact">
+                        Контакты
+                    </Link>
+                    <Link id="login" className="menu-item" to="/login">
+                        Войти
+                    </Link>
+                </Menu>
+            </div>
         </nav>
     );
 };
