c7034813759a624ff0c1e242e346ea9c3ea794e8
AsanbaevBaitemir <baitemir1706@gmail.com>
2024-06-01 10:30:31 +0600

    transactions from back

--- src/pages/TransactionPage/TransactionPage.jsx
+++ src/pages/TransactionPage/TransactionPage.jsx
@@ -1,39 +1,85 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import AppCard from "../../components/AppCard/AppCard";
-import classes from './TransactionPage.module.scss'
+import classes from "./TransactionPage.module.scss";
 import axios from "axios";
-import {api} from "../../store/slices/authSlice.js";
-import Transaction from "../../components/Transaction/Transaction/Transaction.jsx";
+import { api } from "../../store/slices/authSlice.js";
+import { Table } from "antd";
 
 export default function TransactionPage() {
-    const [apps, setApps] = useState([]);
-    const [data, setData] = useState([]);
+  const [apps, setApps] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    try {
+      axios
+        .get(`${api}/app/all/analytics`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        })
+        .then((res) => {
+          setApps(res.data);
+        });
+      axios
+        .get(`${api}/transaction/all`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        })
+        .then((res) => {
+          setTransactions(res.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
-    useEffect(() => {
-        try {
-            axios.get(`${api}/app/all`).then((res) => {
-                setApps(res.data);
-            })
-        } catch (err) {
-            console.log(err);
-        }
-    }, []);
+  console.log(apps);
+  const columns = [
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+    },
+    {
+      title: "App",
+      dataIndex: "app",
+      key: "app",
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Full sum",
+      dataIndex: "full_sum",
+      key: "full_sum",
+    },
+    {
+      title: "Created at",
+      dataIndex: "created_at",
+      key: "created_at",
+    },
+  ];
+  const appsDict = apps.reduce((acc, app) => {
+    acc[app.id] = app.title;
+    return acc;
+  }, {});
+  const newTransactions = transactions.map((transaction) => {
+    return {
+      ...transaction,
+      app: appsDict[transaction.app] || transaction.app,
+    };
+  });
 
-    console.log(apps)
-
-
-    return (
-        <div className={classes.TransactionPage} style={{paddingTop: '120px'}}>
-
-            <div className={classes.apps}>
-                {apps.map((app, index) => {
-                    return <AppCard key={index} data={app}/>
-                })}
-            </div>
-
-            {/*<div>*/}
-            {/*    <Transaction/>*/}
-            {/*</div>*/}
-        </div>
-    );
+  return (
+    <div className={classes.TransactionPage} style={{ paddingTop: "120px" }}>
+      <div className={classes.apps}>
+        {apps.map((app, index) => {
+          return <AppCard key={index} data={app} />;
+        })}
+      </div>
+      <Table dataSource={newTransactions} columns={columns} />
+    </div>
+  );
 }
